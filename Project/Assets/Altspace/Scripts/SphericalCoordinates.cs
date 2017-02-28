9c6a454863fb92066561b3595990b8d2b13730ff
psumadhish <sumadhish@gmail.com>
2017-02-27 20:52:07 -0800

    AltVR - 3d Cursor Code

---
+++ Project/Assets/Altspace/Scripts/SphericalCoordinates.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using UnityEngine;
+
+public class SphericalCoordinates
+{
+	public bool loopPolar = true;
+
+	public bool loopElevation;
+
+	private float _radius;
+
+	private float _polar;
+
+	private float _elevation;
+
+	private float _minRadius;
+
+	private float _maxRadius;
+
+	private float _minPolar;
+
+	private float _maxPolar;
+
+	private float _minElevation;
+
+	private float _maxElevation;
+
+	public float radius
+	{
+		get
+		{
+			return _radius;
+		}
+		private set
+		{
+			 _radius = Mathf.Clamp(value,  _minRadius,  _maxRadius);
+			 _radius = ((!float.IsNaN( _radius)) ?  _radius :  _minRadius);
+		}
+	}
+
+	public float polar
+	{
+		get
+		{
+			return  _polar;
+		}
+		private set
+		{
+			 _polar = ((! loopPolar) ? Mathf.Clamp(value,  _minPolar,  _maxPolar) : Mathf.Repeat(value,  _maxPolar -  _minPolar));
+			 _polar = ((!float.IsNaN( _polar)) ?  _polar :  _minPolar);
+		}
+	}
+
+	public float elevation
+	{
+		get
+		{
+			return  _elevation;
+		}
+		private set
+		{
+			 _elevation = ((! loopElevation) ? Mathf.Clamp(value,  _minElevation,  _maxElevation) : Mathf.Repeat(value,  _maxElevation -  _minElevation));
+			 _elevation = ((!float.IsNaN( _elevation)) ?  _elevation :  _minElevation);
+		}
+	}
+
+	public Vector3 toCartesian
+	{
+		get
+		{
+			float num =  radius * Mathf.Cos( elevation);
+			return new Vector3(num * Mathf.Cos( polar),  radius * Mathf.Sin( elevation), num * Mathf.Sin( polar));
+		}
+	}
+
+	public SphericalCoordinates()
+	{
+	}
+
+	public SphericalCoordinates(float radius, float polar, float elevation, float minRadius = 1f, float maxRadius = 20f, float minPolar = 0f, float maxPolar = 6.28318548f, float minElevation = 0f, float maxElevation = 1.04719758f)
+	{
+		 _minRadius = minRadius;
+		 _maxRadius = maxRadius;
+		 _minPolar = minPolar;
+		 _maxPolar = maxPolar;
+		 _minElevation = minElevation;
+		 _maxElevation = maxElevation;
+		 SetRadius(radius);
+		 SetRotation(polar, elevation);
+	}
+
+	public SphericalCoordinates(Transform T, float minRadius = 1f, float maxRadius = 20f, float minPolar = 0f, float maxPolar = 6.28318548f, float minElevation = 0f, float maxElevation = 1.04719758f) : this(T.position, minRadius, maxRadius, minPolar, maxPolar, minElevation, maxElevation)
+	{
+	}
+
+	public SphericalCoordinates(Vector3 cartesianCoordinate, float minRadius = 1f, float maxRadius = 20f, float minPolar = 0f, float maxPolar = 6.28318548f, float minElevation = 0f, float maxElevation = 1.04719758f)
+	{
+		 _minRadius = minRadius;
+		 _maxRadius = maxRadius;
+		 _minPolar = minPolar;
+		 _maxPolar = maxPolar;
+		 _minElevation = minElevation;
+		 _maxElevation = maxElevation;
+		 FromCartesian(cartesianCoordinate);
+	}
+
+	public SphericalCoordinates FromCartesian(Vector3 cartesianCoordinate)
+	{
+		if (cartesianCoordinate.x == 0f)
+		{
+			cartesianCoordinate.x = 1.401298E-45f;
+		}
+		 radius = cartesianCoordinate.magnitude;
+		 polar = Mathf.Atan(cartesianCoordinate.z / cartesianCoordinate.x);
+		if (cartesianCoordinate.x < 0f)
+		{
+			 polar += 3.14159274f;
+		}
+		 elevation = Mathf.Asin(cartesianCoordinate.y /  radius);
+		return this;
+	}
+
+	public SphericalCoordinates RotatePolarAngle(float x)
+	{
+		return  Rotate(x, 0f);
+	}
+
+	public SphericalCoordinates RotateElevationAngle(float x)
+	{
+		return  Rotate(0f, x);
+	}
+
+	public SphericalCoordinates Rotate(float newPolar, float newElevation)
+	{
+		return  SetRotation( polar + newPolar,  elevation + newElevation);
+	}
+
+	public SphericalCoordinates SetPolarAngle(float x)
+	{
+		return  SetRotation(x,  elevation);
+	}
+
+	public SphericalCoordinates SetElevationAngle(float x)
+	{
+		return  SetRotation( polar, x);
+	}
+
+	public SphericalCoordinates SetRotation(float newPolar, float newElevation)
+	{
+		 polar = newPolar;
+		 elevation = newElevation;
+		return this;
+	}
+
+	public SphericalCoordinates TranslateRadius(float x)
+	{
+		return  SetRadius( radius + x);
+	}
+
+	public SphericalCoordinates SetRadius(float rad)
+	{
+		 radius = rad;
+		return this;
+	}
+
+	public override string ToString()
+	{
+		return string.Concat(new object[]
+			{
+				"[Radius] ",
+				 radius,
+				". [Polar] ",
+				 polar,
+				". [Elevation] ",
+				 elevation,
+				"."
+			});
+	}
+}
---
+++ Project/Assets/Altspace/Scripts/SphericalCursorModule.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class SphericalCursorModule : MonoBehaviour {
+	// This is a sensitivity parameter that should adjust how sensitive the mouse control is.
+	public float Sensitivity;
+
+	// This is a scale factor that determines how much to scale down the cursor based on its collision distance.
+	public float DistanceScaleFactor;
+	
+	// This is the layer mask to use when performing the ray cast for the objects.
+	// The furniture in the room is in layer 8, everything else is not.
+	private const int ColliderMask = (1 << 8);
+
+	// This is the Cursor game object. Your job is to update its transform on each frame.
+	private GameObject Cursor;
+
+	// This is the Cursor mesh. (The sphere.)
+	private MeshRenderer CursorMeshRenderer;
+
+	// This is the scale to set the cursor to if no ray hit is found.
+	private Vector3 DefaultCursorScale = new Vector3(10.0f, 10.0f, 10.0f);
+
+	// Maximum distance to ray cast.
+	private const float MaxDistance = 100.0f;
+
+	// Sphere radius to project cursor onto if no raycast hit.
+	private const float SphereRadius = 1000.0f;
+
+	private SphericalCoordinates sphericalCoordinates;
+
+
+	void Awake() {
+		// Referenced from http://wiki.unity3d.com/index.php/SphericalCoordinates
+		sphericalCoordinates = new SphericalCoordinates(1000f, (Mathf.PI / 2f), 0f, 0f, 20f, 0f, -(Mathf.PI * 2f), -(Mathf.PI / 3f), (Mathf.PI / 3f));
+		Cursor = transform.Find("Cursor").gameObject;
+		CursorMeshRenderer = Cursor.transform.GetComponentInChildren<MeshRenderer>();
+        CursorMeshRenderer.GetComponent<Renderer>().material.color = new Color(0.0f, 0.8f, 1.0f);
+    }	
+
+
+	void Update()
+	{
+		// TODO: Handle mouse movement to update cursor position.
+		Vector2 mouseVector = Vector2.zero;
+		mouseVector.x = -Input.GetAxis("Mouse X") * Sensitivity;
+		mouseVector.y = Input.GetAxis("Mouse Y") * Sensitivity;
+		sphericalCoordinates.SetRotation(sphericalCoordinates.polar + mouseVector.x, sphericalCoordinates.elevation + mouseVector.y);
+
+		// TODO: Perform ray cast to find object cursor is pointing at.
+		
+		// TODO: Update cursor transform.
+
+		var cursorHit = new RaycastHit();/* Your cursor hit code should set this properly. */;
+		Vector3 exactPosition = gameObject.transform.position;
+		Vector3 objectDirection = transform.TransformPoint(sphericalCoordinates.toCartesian) - exactPosition;
+		Ray cameraRay = new Ray(exactPosition, objectDirection);
+
+		RaycastHit finalTarget;
+		if (Physics.Raycast(cameraRay, out finalTarget, 100f, 256))
+		{
+			Cursor.transform.position = finalTarget.point;
+			float num = (finalTarget.distance * DistanceScaleFactor + 1f) / 2f;
+			Cursor.transform.localScale = new Vector3(num, num, num);	
+		}
+		else
+		{
+			Cursor.transform.localPosition = sphericalCoordinates.toCartesian;
+			Cursor.transform.localScale = DefaultCursorScale;
+		}
+
+		cursorHit = finalTarget;
+
+		// Update highlighted object based upon the raycast.
+		if (cursorHit.collider != null)
+		{
+			Selectable.CurrentSelection = cursorHit.collider.gameObject;
+		}
+		else
+		{
+			Selectable.CurrentSelection = null;
+		}
+	}
+}
